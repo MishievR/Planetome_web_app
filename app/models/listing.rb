7ff38938143e286b7a9eaa619b7f1dd233cb15d6
Planetome Web App <roman@plametome.com>
2017-04-19 17:47:13 +0300

    add listing and category assosiation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ActiveRecord::Base
-
   belongs_to :user
-
+  has_many :listing_categories
+  has_many :categories, through: :listing_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :user_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414134653) do
+ActiveRecord::Schema.define(version: 20170419121546) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "listing_categories", force: :cascade do |t|
+    t.integer "listing_id"
+    t.integer "category_id"
   end
 
   create_table "listings", force: :cascade do |t|
