6c8b34aef3a37ba19e72e610f8691310c52c59e0
Planetome Web App <roman@plametome.com>
2017-04-23 23:09:02 +0300

    Fix listings views

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,5 +5,5 @@ class Listing < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :user_id, presence: true
-
+  validates :category_ids, presence: true
 end
