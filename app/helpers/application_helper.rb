7277362d3fbe79262945eaf8233f3ef3ae96cc38
Planetome Web App <roman@plametome.com>
2017-04-24 20:55:01 +0300

    _listing design fix, show info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def gravatar_for(user, options = {size: 60})
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=https%3A%2F%2Fc2.staticflickr.com%2F4%2F3928%2F32947863914_f3f71996ef_o.png"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=https%3A%2F%2Fc2.staticflickr.com%2F5%2F4185%2F33855526460_f36e2fc642_o.png"
     image_tag(gravatar_url, size: "60", class: "img-circle")
   end
 end
