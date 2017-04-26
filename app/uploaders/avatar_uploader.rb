5be9ccada54a649bc771a8a6a14f7643862b0f88
Planetome Web App <roman@plametome.com>
2017-04-26 15:33:32 +0300

    Add profile image to edit

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -31,18 +31,24 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [50, 50]
+    process resize_to_fill: [50, 50]
   end
 
   version :small do
-    process :resize_to_fit => [150, 150]
+    process :resize_to_fill => [150, 150]
     process :quality => 100
   end
 
   version :medium do
-    process :resize_to_fit => [200, 200]
+    process :resize_to_fill => [200, 200]
     process :quality => 100
   end
+
+  version :large do
+    process :resize_to_fill => [600, 600]
+    process :quality => 100
+  end
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
