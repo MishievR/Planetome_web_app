f5b6bfcb1c6e3f05f23d1b0c3241ca9e81870091
Planetome Web App <roman@plametome.com>
2017-05-02 15:44:51 +0300

    db optimtzation

--- db/migrate/20170421145658_devise_create_users.rb
+++ db/migrate/20170421145658_devise_create_users.rb
@@ -30,7 +30,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.boolean  "admin",                  default: false
+      t.string   "username"
+      t.string   "first_name"
+      t.string   "last_name"
+      t.integer  "age"
+      t.integer  "type"
+      t.string   "location_town"
+      t.string   "inerest_main"
+      t.text     "about"
+      t.string   "avatar"
       t.timestamps null: false
     end
 
